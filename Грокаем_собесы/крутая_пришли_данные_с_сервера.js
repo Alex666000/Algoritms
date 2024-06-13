abbc6f626b4fa4a6b50bd96e1a3a40a373b9a9cf
Alex666000 <bogdanov777000@mail.ru>
2024-06-13 09:41:42 +0300

    ДНК задача

---
+++ Грокаем_собесы/крутая_пришли_данные_с_сервера.js
@@ -0,0 +1,17 @@
+// Задачи к уроку 8
+//
+// 1) Есть массив объектов представляющих данные о разработчиках, которые подписались на участие в встрече по программированию
+//
+// Ваша задача — вернуть количество JavaScript-разработчиков, приезжающих из Европы .
+//
+//   Например, учитывая следующий список:
+//   let list1 = [
+//     { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//     { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//     { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//     { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+//   ];
+//
+// Ваша функция должна возвращать число 1.
+//
+// Если разработчиков JavaScript из Европы нет, ваша функция должна вернуть 0.
