7ae8ab9cbc56ed208bbaf11b810ae9492f3c04c5
Alex666000 <bogdanov777000@mail.ru>
2024-10-13 20:05:19 +0300

    fibonacci

--- Грокаем_собесы/грокаем/рекурсия.js
+++ Грокаем_собесы/грокаем/рекурсия.js
@@ -6,9 +6,25 @@
 
 function factorial(n) {
   if (n === 0) return 1
-
+  // console.log(this) - this тут есть но мы его не используем
   // сначала работает выражение справа в js потом слева
   return n * factorial(n - 1) // замыкание n - сохраняется не сбрасывается - сначала n=4 потом n=3 и так до выхода из рекурсии
 }
 
 console.log(factorial(4))
+// Чему равен factorial от (4)? = 4 * factorial(3)
+
+// 2 "фибоначчи":
+// console. log (factorial(-4));
+// fibonacci (0) = 0;
+// fibonacci (1) = 1;
+// fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2), n > 1;
+
+function fibonacci(n) {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+    console.log(fibonacci(6))
+  }
+}
