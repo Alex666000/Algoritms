8a16f79289143545d7cf9cae38211d561ff913ba
Alex666000 <bogdanov777000@mail.ru>
2024-10-13 19:34:59 +0300

    стэк

--- Грокаем_собесы/грокаем/стек_и_очередь.js
+++ Грокаем_собесы/грокаем/стек_и_очередь.js
@@ -1,15 +1,15 @@
-// 1 - стек
+// 1 - стек и очередь while обходить удобнее всего
 const flatten = (...args) => {
   const result = []
-  const stack = [...args] // чтобы не мутировать исходный массив
+  const stack = [...args] // чтобы не мутировать исходный массив делаем дубликат - тк потом мутирующие методы массива мутируют исходный args
 
   while (stack.length) { // пока стек существует
     const firstElem = stack.shift()
     if (Array.isArray(firstElem)) {
       stack.unshift(...firstElem)
-    } else {
-      result.push(firstElem)
+      continue // так удобнее - выйдем в след цикл чтобы else {} не писать
     }
+    result.push(firstElem)
 
   }
 
@@ -19,13 +19,4 @@ const flatten = (...args) => {
 console.log(flatten(-1, [2, [[3]]], 4, 5, [6, [7]]));
 // console.log(flatten('a', ['b', 2], 3, null, [[4], ['c']]));
 
-// 2 очередь: вычислить максимальное время которое потребуется чтобы обслужилась вся очередь в кинотеатр
-function queueTime(customers, n) {
-  const queue = [...customers]
-}
-
-console.log(queueTime([1], 1), 0);
-console.log(queueTime([1,2,3,4], 1), 10);
-console.log(queueTime([2,2,3,3,4,4], 2), 9);
-console.log(queueTime([1,2,3,4,5], 100), 5);
 
