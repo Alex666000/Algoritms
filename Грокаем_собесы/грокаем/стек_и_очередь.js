8e04fac5cab6485e5e4e53dfabb3a580e488906e
Alex666000 <bogdanov777000@mail.ru>
2024-10-13 17:54:53 +0300

    плоскии массив

--- Грокаем_собесы/грокаем/стек_и_очередь.js
+++ Грокаем_собесы/грокаем/стек_и_очередь.js
@@ -1,4 +1,23 @@
 // "плоский массив" - вернуть надо развернутый массив
-function flatten() {
+// по факту args и будет наш стэк
+function flatten(...args) {
+  const result = []
+
+  while (args.length) {
+    // берем первый элем из стэка и одновременно удаляем из него
+    const el = args.shift()
+    if (Array.isArray(el)) {
+      // если массив то мы его развернем и в самое начало положим
+      args.unshift(...el)
+      continue // далее возвращаемся в цикл while снова - чтобы не писать else - континье красивее
+    }
+    result.push(el) // если это не массив пушим в результирующий
+  }
+
+  return result
 
 }
+
+
+console.log(flatten(-1, [2, [[3]]], 4, 5, [6, [7]]));
+// console.log(flatten('a', ['b', 2], 3, null, [[4], ['c']]));
