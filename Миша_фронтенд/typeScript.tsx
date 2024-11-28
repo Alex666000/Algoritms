3f92261fc8e8f8c9dd560d63eeeccbe0b3013bdd
Alex666000 <bogdanov777000@mail.ru>
2024-11-28 19:07:26 +0300

    тротл + дебаунс

--- Миша_фронтенд/typeScript.tsx
+++ Миша_фронтенд/typeScript.tsx
@@ -13,12 +13,18 @@
 //   );
 // };
 //
-// // Поправить типы в блоке ниже
-// // Прописать типы компонента OtherFC в проп Component компонента ContainerFC
-// // Это позволит подсказать, какие еще пропы мы можем передать в ContainerFC
-// // Помимо собственных пропов ContainerFC с правильным указанием типа этих пропов
 //
-// /** Начало блока в котором можно редактировать и дополнять типы */
+// Мы рассматриваем компонент ContainerFC, который принимает в пропсах другой компонент
+// (например, OtherFC) и его пропсы. Цель — сделать так, чтобы TypeScript автоматически
+// проверял, что переданные пропсы соответствуют требованиям переданного компонента.
+// Компонент ContainerFC должен:
+//
+// Принимать компонент (Component) через проп Component.
+// Принимать пропсы, которые ожидает переданный компонент (Component).
+// Работать корректно для любого компонента, не зная заранее, какие пропсы он ожидает.
+// ContainerFC получает OtherFC в качестве Component.
+// Но OtherFC ожидает пропсы name и age. Если мы передадим что-то другое (например, role), TypeScript должен показать ошибку.
+
 // type IProps = {
 //   Component: any;
 //   height: number;
@@ -55,3 +61,67 @@
 // };
 
 // РЕШЕНИЕ:
+// import "./styles.css";
+// import React, { useEffect, useState, FC } from "react";
+//
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <AnyComponent1 />
+//       <AnyComponent2 />
+//       <AnyComponent3 />
+//     </div>
+//   );
+// }
+//
+// type OtherProps = {
+//   name: string;
+//   age: number;
+// };
+//
+// const OtherFC: FC<OtherProps> = ({ age, name }) => {
+//   return (
+//     <div>
+//       {name} {age}
+//     </div>
+//   );
+// };
+//
+// // Мы рассматриваем компонент ContainerFC, который принимает в пропсах другой компонент
+// // (например, OtherFC) и его пропсы. Цель — сделать так, чтобы TypeScript автоматически
+// // проверял, что переданные пропсы соответствуют требованиям переданного компонента.
+//
+// type IProps<T> = {
+//   Component: FC<T>;
+//   height: number;
+//   // & T: Мы объединяем пропсы, которые ожидает ContainerFC (height, Component), с пропсами,
+//   // которые ожидает переданный компонент (Component).
+// } & T; // свои пропсы + пропсы что ожидает OtherFC
+//
+// const ContainerFC: FC<IProps> = (props) => {
+//   const { Component, height, ...rest } = props;
+//
+//   return (
+//     <div style={{ height: `${height}px` }}>
+//       {/* <Component {...rest} /> */}
+//       <Component {...(rest as T)} />
+//     </div>
+//   );
+// };
+//
+// /** Тут не должно быть ошибок типов */
+// const AnyComponent1 = () => {
+//   return <ContainerFC height={5} Component={OtherFC} name="Max" age={30} />;
+// };
+//
+// const AnyComponent2 = () => {
+//   return <ContainerFC height={5} Component={OtherFC} name={30} age="Max" />;
+// };
+//
+// const AnyComponent3 = () => {
+//   return <ContainerFC height={5} Component={OtherFC} role="Max" />;
+// };
+//
+// const AnyComponent4 = () => {
+//   return <ContainerFC height={5} Component={OtherFC} name="Max" />;
+// };
