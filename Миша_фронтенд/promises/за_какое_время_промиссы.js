b2e089fe8c55b80763ba5f5bc09dcb9912da8346
Alex666000 <bogdanov777000@mail.ru>
2024-12-02 11:19:15 +0300

    ax

---
+++ Миша_фронтенд/promises/за_какое_время_промиссы.js
@@ -0,0 +1,29 @@
+// За какое время выполняться add1() и add2()?
+
+function resolveAfter2Seconds(x) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(x);
+    }, 2000);
+  });
+}
+
+async function add1(x) {
+  const a = await resolveAfter2Seconds(20); // 2 сек. - пока код не выполниться интерпретатор дальше не поидет к след строке
+  const b = await resolveAfter2Seconds(30); // 2 сек. - Итог: 2 + 2 = 4 сек.
+  return x + a + b;
+}
+
+async function add2(x) {
+  // тут запускается без await значит одновременно полетят - как PromiseAll()
+  const promise_a = resolveAfter2Seconds(20);
+  const promise_b = resolveAfter2Seconds(30);
+  // с такои записью: (await promise_a) + (await promise_b); одновременно 2 промиса запустяться - будет 2 сек
+  // такои синтакси переписать на Promise.all()...
+  const nums = await Promise.all([promise_a, promise_b])
+  return x + nums[0] + nums[1];
+  // return x + (await promise_a) + (await promise_b)
+}
+
+add1(10).then(console.log);
+add2(20).then(console.log);
