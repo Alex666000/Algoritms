6b1bf7b74cce6f5e286ab465b3b5bee63be08fc6
Alex666000 <bogdanov777000@mail.ru>
2024-12-04 18:30:41 +0300

    тротл + дебаунс

--- trains.js
+++ trains.js
@@ -1,20 +1,3 @@
 // Сколько раз элемент встречается в массиве: Ответ в виде: {kiwi: 2}
 const fruits = ['kiwi', 'apple', 'kiwi', 'kiwi']
 
-const countElem = (array) => {
-  const count = {}
-
-  array.forEach(item => { // // когда пробегаемся "форич" всегда так сносим вниз фигурные скобки
-    if (count[item]) {
-      count[item]++
-    }
-
-    if (!count[item]) {
-      count[item] = 1
-    }
-  })
-
-  return count
-}
-
-console.log(countElem(fruits))
