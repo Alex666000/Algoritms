6a441de8f892579824ca9f1c59adad31d1ce68c5
Alex666000 <bogdanov777000@mail.ru>
2024-12-09 13:33:53 +0300

    тротл + дебаунс

---
+++ ПРАВИЛА_JS/includes/includes_для_строк_и_массивов.js
@@ -0,0 +1,10 @@
+// "includes" можно применять также и к строке. Этот метод проверяет, содержит ли строка указанную подстроку, и возвращает true или false
+// Синтаксис: string.includes(substring, position)
+// substring — подстрока, которую нужно найти.
+// position (необязательно) — начальная позиция, с которой начинать поиск. По умолчанию равна 0
+/*
+Пример:
+  const arr = [1, 2, 3, 4];
+console.log(arr.includes(3)); // true
+console.log(arr.includes(5)); // false
+ */
