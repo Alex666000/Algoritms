9adba39c76b8ad1883b9b5b9473c1bd3aba5208f
Alex666000 <bogdanov777000@mail.ru>
2024-12-23 09:31:02 +0300

    тротл + дебаунс

---
+++ _Процессы работы_с_HR_тех_собесы/Полезные_ссылки.js
@@ -0,0 +1,9 @@
+/*
+https://m1shafrontend.yonote.ru/share/2b3fb662-1c82-48e2-a77d-d7c16710028e/doc/osnova-5HIMLRNW96 - Самопрезентация + процессы
+https://buildin.ai/share/6cd3bd51-4b3c-4dad-914e-d7775904e5ba?code=GLY9W0 - Самопрезентация
+https://buildin.ai/share/16ad4657-1d87-4714-8ff1-934c4076a9b3?code=GLY9W0 - Легенда
+https://buildin.ai/share/c57fda52-b984-42fd-a983-70e924c7c802?code=GLY9W0 - софт скилы
+https://buildin.ai/share/271616d8-38a4-4732-b49a-138d823e4b24 - Состав команды и их роли
+
+
+ */
